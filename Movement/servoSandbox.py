a2b84eebe475ff0fc83730862f541f285853da1c
MoCoCode <42018716+MoCoCode@users.noreply.github.com>
2018-08-01 22:38:08 -0700

    Update servoSandbox.py

--- Movement/servoSandbox.py
+++ Movement/servoSandbox.py
@@ -1,1 +1,10 @@
 # Sandbox for testing servos
+
+#import servo module
+import Adafruit_PCA9685
+
+# Initialise the PCA9685 using the default address (0x40).
+pwm = Adafruit_PCA9685.PCA9685()
+
+# Initialise the PCA9685 using the default address (0x40).
+pwm = Adafruit_PCA9685.PCA9685()
