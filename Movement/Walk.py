a56a0130a4cc068e264030f73fc5286d57969238
MoCoCode <42018716+MoCoCode@users.noreply.github.com>
2018-08-11 11:36:12 -0700

    Create Walk.py
    Initial commit

---
+++ Movement/Walk.py
@@ -0,0 +1,14 @@
+# Walk Forward
+
+from __future__ import division
+import time
+
+import Adafruit_PCA9685
+
+# Uncomment to enable debug output.
+#import logging
+#logging.basicConfig(level=logging.DEBUG)
+
+# Initialise the PCA9685 using the default address (0x40).
+pwm = Adafruit_PCA9685.PCA9685()
+
